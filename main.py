093fd22630af8c29ae21b773ca5e8882570a2226
Darius Foo <darius@srcclr.com>
2016-09-17 16:17:50 +0800

    Reproduce IOException with __init__.py directory

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-from jwt import algorithms
-from django.utils import formats
-from rsa import cli
-from requests import sessions
+from test import *
 
-if __name__ == '__main__':
-    formats.get_format()
-    algorithms.HMACAlgorithm.prepare_key()
-    cli.VerifyOperation.perform_operation()
-    sessions.SessionRedirectMixin.resolve_redirects()
